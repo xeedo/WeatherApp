0a363df287b1279ceb812a99f33eee6a1344a257
Przemysław Grądzik <pggradzik@gmail.com>
2017-10-18 22:36:01 +0200

    Showing user current location

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,24 @@
 for (var i = 0; i < 10; i++) {
   $(".more-info").append("<p>Test</p>");
 }
+
+function getLocation() {
+  if (!navigator.geolocation) {
+    return null;
+  }
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+function showPosition(position) {
+  var latlng = "latlng=" + position.coords.latitude + "," + position.coords.longitude;
+  var resultType = "&result_type=locality";
+  var apiKey = "&key=AIzaSyACb2HlwvxHmQJ7RCxjSuQL-FGp7SH23Vg";
+
+  $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?"
+    + latlng
+    + resultType
+    + apiKey
+    , function(data) {
+      $("#location").html(data.results[0].formatted_address);
+  })
+}
